a08fecc212890c43e85dcc639bb72690b2b22ca1
Aatir Nadim <aatir.nadim@gmail.com>
2022-11-13 00:18:34 +0530

    save the user in the data layer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Login from './components/Login';
 import Player from './components/Player'
 import { getTokenFromResponse } from './components/spotify';
-
+import { useStateValue } from './components/Stateprovider';
 import SpotifyWebApi from 'spotify-web-api-js';
 //supports a bunch of functionalities
 const spot = new SpotifyWebApi();
@@ -12,6 +12,7 @@ const spot = new SpotifyWebApi();
 function App() {
   //runs the code based on some condition
   const [token, setToken] = useState(null) //temporary storage, session storage, all lost while reloading
+  const [{}, dispatch] = useStateValue();
 
   useEffect(() => {
     const hash = getTokenFromResponse();
@@ -24,6 +25,14 @@ function App() {
       spot.getMe() //returns a promise
       .then((user) => {
         console.log(user);
+        dispatch ({
+          type : 'SET_USER',
+          user : user, 
+        })
+      })
+      .catch(err => {
+        alert('An error has occured. Please try again later or contact the service provider if the issue persists');
+        console.error(err);
       })
     }
   }, []);
--- src/components/Stateprovider.js
+++ src/components/Stateprovider.js
@@ -7,4 +7,4 @@ export const Stateprovider = ({initialState, reducer, children}) => {
     </StateContext.Provider>
 }
 
-export default Stateprovider;
+export const useStateValue = () => useContext(StateContext);
