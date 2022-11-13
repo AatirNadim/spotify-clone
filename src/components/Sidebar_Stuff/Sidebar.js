c154ef584acc5110337968b16fe227158f261cd7
Aatir Nadim <aatir.nadim@gmail.com>
2022-11-13 15:55:39 +0530

    glitches handled

--- src/components/Sidebar_Stuff/Sidebar.js
+++ src/components/Sidebar_Stuff/Sidebar.js
@@ -6,13 +6,14 @@ import HomeIcon from '@mui/icons-material/Home';
 import SearchIcon from '@mui/icons-material/Search';
 import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
 function Sidebar() {
+  // console.log({desc : 'Icon', type : typeof(<HomeIcon/>)})
   return (
     // <div className='sidebar__body'>this is the sidebar lbhgsdfgfjgjgjgvcgdzjfjcgvjhbvhjzgfcvyumdsgfchjmsgffnghnbvdvjbfvczsxhvdsjhfbhdsfvbchjsdbvfcjsfbhyjdbdsd </div>
     <div className = {styles.sidebar__body}>
       <img className = {styles.spotify__logo}src = "https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt = "spotify__logo"/> 
-      <SidebarOptions title = {'Home'} Icon = {<HomeIcon/>}/>
-      <SidebarOptions title = {'Search'} Icon = {<SearchIcon/>}/>
-      <SidebarOptions title = {'Your Library'} Icon = {<LibraryMusicIcon/>}/>
+      <SidebarOptions title = {'Home'} Icon = {HomeIcon}/>
+      <SidebarOptions title = {'Search'} Icon = {SearchIcon}/>
+      <SidebarOptions title = {'Your Library'} Icon = {LibraryMusicIcon}/>
     </div>
     // <div className = "spotify__logo">
       /* */
--- src/components/Sidebar_Stuff/SidebarOptions.js
+++ src/components/Sidebar_Stuff/SidebarOptions.js
@@ -2,9 +2,13 @@ import React from 'react'
 // import '../../styles/SidebarOptions.module.css'
 import styles from '../../styles/SidebarOptions.module.css'
 const SidebarOptions = ({ title , Icon }) => {
+    // console.log({ desc : 'icon', type : typeof(Icon)});
     return (
         <div className={styles.Sidebar_Options__main}>
-            <span>{Icon}</span><p>{title}</p>
+            {Icon && <Icon className = {styles.Sidebar_Options__Icon}/>}
+            {Icon? <h4 style={{fontWeight : '700'}}>{title}</h4>:<p>{title}</p>}
+            {/* <p>thihterhtirh</p> */}
+            {/* this element does not get styled in case the css is not defined for it specifically, the css of the parent element has lower priority than index.css */}
         </div>
     );
 }
