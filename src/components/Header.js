90d842848a41f8aee1811270fa3e46ff30e38f64
Aatir Nadim <aatir.nadim@gmail.com>
2022-11-15 20:36:07 +0530

    done with the header pulling

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ import Avatar from '@mui/material/Avatar';
 import { useStateValue } from './Stateprovider'
 const Header = ({spotify}) => {
     const [{user}, dispatch] = useStateValue();
+    console.log('user -->\n', {user});
     return (
         <div className = {styles.header__main}>
             <div className = {styles.header__left}>
@@ -16,11 +17,11 @@ const Header = ({spotify}) => {
             </div>
             <div className = {styles.header__right}>
                 {/* <AccountCircleIcon style = {{marginRight : '10px'}}/> */}
-                <Avatar src = {user?.images[0]?.url} alt = 'img'
+                <Avatar src = {user?.images[0]?.url} alt = {user?.display_name}
                     style = {{marginRight : '10px'}}
                 />
                 {/* {'staticLarryBurlonce'} */}
-                {user?.display_name}
+                <h4>{user?.display_name}</h4>
             </div>
         </div>
 
